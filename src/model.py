4b0b80d891129bc8570b88371f383cccdc5c886e
gasiortomasz <gasior.tt@gmail.com>
2020-04-21 11:18:59 +0200

    Add simple model

---
+++ src/model.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SimpleModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 64, 3)
+        self.conv4 = nn.Conv2d(64, 64, 3)
+        self.linear = nn.Linear(64*13*13, 28)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
+        return x
