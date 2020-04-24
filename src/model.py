69a6ad645f45600fa91aa6148ac8b83c98b974d5
gasiortomasz <gasior.tt@gmail.com>
2020-04-24 14:16:17 +0200

    Update model: add probs head

--- src/model.py
+++ src/model.py
@@ -2,13 +2,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class SimpleModel(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes=28):
         super().__init__()
+        self.num_classes = num_classes
         self.conv1 = nn.Conv2d(3, 32, 3)
         self.conv2 = nn.Conv2d(32, 64, 3)
         self.conv3 = nn.Conv2d(64, 64, 3)
         self.conv4 = nn.Conv2d(64, 64, 3)
-        self.linear = nn.Linear(64*13*13, 28)
+        self.linear = nn.Linear(64 * 13 * 13, self.num_classes)
+        self.softmax = nn.Softmax(1)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -25,4 +27,4 @@ class SimpleModel(nn.Module):
         x = F.max_pool2d(x, 2)
         x = x.view(x.size(0), -1)
         x = self.linear(x)
-        return x
+        return x, self.softmax(x)
