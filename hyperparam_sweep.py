13f3115dc4f5cc64e213e7106e4000af3066d4b9
gasiortomasz <gasior.tt@gmail.com>
2020-04-28 19:10:56 +0200

    Move sweep script outside src

--- hyperparam_sweep.py
+++ hyperparam_sweep.py
@@ -16,6 +16,8 @@ def parse_args():
   parser.add_argument('--train-batch-size', type=int, default=32, help='Training batch size')
   parser.add_argument('--test-batch-size', type=int, default=256, help='Testing batch size')
   parser.add_argument('--optimizer', choices=['SGD'], default='SGD')
+  parser.add_argument('--momentum', type=float, default=0.0)
+  parser.add_argument('--weight-decay', type=float, default=0.0001)
   parser.add_argument('--data-dir', help='Path to data folders', required=True)
   parser.add_argument('--use-cpu', action='store_true')
   return parser.parse_args()
@@ -35,18 +37,17 @@ if __name__ == '__main__':
   args = parse_args()
 
   for lr in args.learning_rates:
-    os.remove('job.sh')
-
-    cmd = prepare_entropy_command(args)
-    cmd += f'python3 src/main.py --data-dir {args.data_dir} --learning-rate {lr} '
+    # os.remove('job.sh')
+    # cmd = prepare_entropy_command(args)
+    cmd = f'python3 src/main.py --data-dir {args.data_dir} --learning-rate {lr} '
     cmd += f'--optimizer {args.optimizer} --epochs {args.epochs} '
     cmd += f'--train-batch-size {args.train_batch_size} --test-batch-size {args.test_batch_size} '
+    cmd += f'--momentum {args.momentum} --weight-decay {args.weight_decay} '
     cmd += f'--use-cpu' if args.use_cpu else ''
     cmd += '\n'
+    subprocess.run(cmd, shell=True)
 
-    with open('job.sh', 'w') as f:
-      f.write(cmd)
-
+    # with open('job.sh', 'w') as f:
+    #   f.write(cmd)
     # print(f'Executing command: {cmd}')
-
-    subprocess.run('sbatch job.sh', shell=True)
+    # subprocess.run('sbatch job.sh', shell=True)
